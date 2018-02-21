5b2a0916a51d1dec83789456b64ac6f87724e118
Phluxxed <Tempest13@gmail.com>
2018-02-21 18:21:15 +1000

    v5 edits
    Updated as I'm following along in the course.

--- js/colorGame.js
+++ js/colorGame.js
@@ -30,7 +30,7 @@ easyBtn.addEventListener("click", function() {
             squares[i].style.display = "none";
         }
     }
-    h1.style.backgroundColor = "#232323";
+    h1.style.backgroundColor = "steelblue";
 });
 
 hardBtn.addEventListener("click", function() {
@@ -49,7 +49,7 @@ hardBtn.addEventListener("click", function() {
         squares[i].style.backgroundColor = colors[i];
         squares[i].style.display = "block";
     }
-    h1.style.backgroundColor = "#232323";
+    h1.style.backgroundColor = "steelblue";
 });
 
 resetButton.addEventListener("click", function() {
@@ -63,7 +63,7 @@ resetButton.addEventListener("click", function() {
     for (var i = 0; i < squares.length; i++) {
         squares[i].style.backgroundColor = colors[i];
     }
-    h1.style.backgroundColor = "#232323";
+    h1.style.backgroundColor = "steelblue";
     this.textContent = "New Colors";
 });
 
